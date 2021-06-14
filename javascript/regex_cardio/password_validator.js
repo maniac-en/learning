9a0e92882a3836387a62f034c70601884f89b71b
Shivam Mehta <sm.cse17@gmail.com>
2021-06-14 11:33:12 +0530

    refactoring using Regex.prototype.test

--- javascript/regex_cardio/password_validator.js
+++ javascript/regex_cardio/password_validator.js
@@ -1,20 +1,13 @@
 #!/usr/bin/env node
 
 const checkPasswordStrength = input => {
-  let match;
-
   /* Strong pass conditions:
-   * Atleast one lower-case character
-   * Atleast one upper-case character
-   * Atleast one decimal number
+   * Atleast one lower-case character (1st lookaround)
+   * Atleast one upper-case character (2nd lookaround)
+   * Atleast one decimal number (3rd lookaround)
    * Password should be of atleast 8 characters
    */
-
-  if (match = input.match(/^(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z])(?=\D*\d).{8,}$/)) {
-    return true;
-  }
-  else
-    return false;
+  return /^(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z])(?=\D*\d).{8,}$/.test(input);
 }
 
 // test
